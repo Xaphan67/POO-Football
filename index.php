2b030b73e11e4d02506b880f23b301c49726408b
Xaphan67 <cedric.falda@gmail.com>
2023-08-24 16:55:46 +0200

    Ajout : début variables index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+            <link rel="stylesheet" type="text/css" href="style.css" />
+            <title>POO Football</title>
+        </head>
+    <body>
+        <?php
+            // Equipes
+            $equipe1 = new Equipe("Parais Saint-Germain");
+            $equipe2 = new Equipe("Bayern Munich");
+            $equipe3 = new Equipe("Real Madrid");
+        ?>
+    </body>
+</html>
