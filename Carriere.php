a79cfcae8c46aa06d140ff232fb67a7104d86bb4
Xaphan67 <cedric.falda@gmail.com>
2023-08-24 16:55:26 +0200

    Ajout : Classe carrière

---
+++ Carriere.php
@@ -0,0 +1,41 @@
+<?php
+
+class Carriere {
+    private Joueur $joueur;
+    private Equipe $equipe;
+    private int $anneeSaison;
+
+    public function __construct(Joueur $joueur, Equipe $equipe, string $anneeSaison) {
+    $this->joueur = $joueur;
+    $this->equipe = $equipe;
+    $this->anneeSaison = $anneeSaison;
+    }
+
+    public function getJoueur() {
+        return $this->joueur;
+    }
+
+    public function getEquipe() {
+        return $this->equipe;
+    }
+
+    public function getAnneeSaison() {
+        return $this->getAnneeSaison;
+    }
+
+    public function setJoueur(Joueur $joueur) {
+        $this->joueur = $joueur;
+    }
+
+    public function setEquipe(Equipe $equipe) {
+        $this->equipe = $equipe;
+    }
+
+    public function setAnneeSaison(int $anneeSaison) {
+        $this->anneeSaison = $anneeSaison;
+    }
+
+    public function __toString() {
+        return $this->joueur->getNom() . " - " . $this->equipe->getNom() . " (" . $this->anneeSaison . ")";
+    }
+}
